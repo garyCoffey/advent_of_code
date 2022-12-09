1bc79fd6639c5d8fac1f92fb518c6cd84c73d658
Gary coffey <garycoffey@Garys-MacBook-Pro.local>
2022-12-08 21:06:10 -0600

    day2_v1

---
+++ day2/runner_spec.rb
@@ -0,0 +1,25 @@
+require './day2/runner.rb'
+
+describe 'Day2' do
+  context '#result' do
+    context 'part 1' do 
+      let(:result)      { Day2.new.result.split("\n").first }
+
+      it 'should be a string' do
+        expect(result.class).to eq(String)
+      end
+
+      context 'strategy should score 15' do 
+        before do 
+          allow_any_instance_of(Day2).to receive(:file_path).and_return('./day2/data/test_data_part1.txt')
+        end
+
+        let(:calculation) { result.split.last }
+
+        it 'should be 10' do
+          expect(calculation).to eq('15')
+        end
+      end
+    end
+  end
+end
